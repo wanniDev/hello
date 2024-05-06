ddb2a657ac4323da78783e097cf0f6ccc6496cae
Wanni <wannidev0928@gmail.com>
2024-05-06 18:09:36 +0900

    project init

---
+++ src/main/kotlin/me/spring/hello/HelloController.kt
@@ -0,0 +1,12 @@
+package me.spring.hello
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello, AWS!"
+    }
+}
